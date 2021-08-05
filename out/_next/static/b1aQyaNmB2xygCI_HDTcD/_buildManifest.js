9fa72a3376dcf14c76328f9e12c6460f226ade00
Steven Del Rosario <stevendelro@pm.me>
2021-08-05 00:41:32 -0500

    created an out folder for aws deployment

---
+++ out/_next/static/b1aQyaNmB2xygCI_HDTcD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/828-b90ca7e03d7876789dc6.js","static/chunks/pages/index-001ee8aa4c71fa075421.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
