fd3b4007c0f3844280ad760c9cb5b7d0eb25ce2c
Steven Del Rosario <stevendelro@pm.me>
2021-08-04 21:29:58 -0500

    initialize app with proper material-ui theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,89 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider, makeStyles, createTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import React, { useState, useEffect, useMemo } from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
+import MyAppBar from '../components/Layout/MyAppbar';
 
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    backgroundColor:
+      theme.palette.type === 'dark'
+        ? theme.palette.common.defaultDarkBackground
+        : theme.palette.common.defaultLightBackground,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+
+  content: {
+    flexGrow: 1,
+    width: '100vw',
+  },
+}));
+
+export default function MyApp({ Component, pageProps }) {
+  const classes = useStyles();
+  const [darkMode, setDarkMode] = useState(false);
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
+  useEffect(() => {
+    prefersDarkMode ? setDarkMode(true) : setDarkMode(false);
+  }, [prefersDarkMode]);
 
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          primary: {
+            main: '#fff',
+            light: '#fff',
+            dark: '#b2b2b2',
+            contrastText: '#000',
+            accent: '#923f2e',
+          },
+          secondary: {
+            main: '#000',
+            light: '#333',
+            dark: '#000',
+            contrastText: '#ffffff',
+          },
+          common: {
+            defaultDarkBackground: '#303030',
+            defaultLightBackground: '#f5f5f5',
+          },
+          type: darkMode ? 'dark' : 'light',
+        },
+      }),
+    [darkMode]
+  );
+  const toggleDarkMode = () => setDarkMode(!darkMode);
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <title>KUR8</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
+        <div className={classes.root}>
+          <CssBaseline />
+          <MyAppBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Component {...pageProps} />
+          </main>
+        </div>
       </ThemeProvider>
     </React.Fragment>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
-import theme from '../src/theme';
 
 export default class MyDocument extends Document {
   render() {
@@ -9,7 +8,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name="theme-color" content='#fff' />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
@@ -26,43 +25,22 @@ export default class MyDocument extends Document {
 
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with server-side generation (SSG).
-MyDocument.getInitialProps = async (ctx) => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
-  // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
+MyDocument.getInitialProps = async ctx => {
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
 
   ctx.renderPage = () =>
     originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
+      enhanceApp: App => props => sheets.collect(<App {...props} />),
+    })
 
-  const initialProps = await Document.getInitialProps(ctx);
+  const initialProps = await Document.getInitialProps(ctx)
 
   return {
     ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-  };
-};
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  }
+}
