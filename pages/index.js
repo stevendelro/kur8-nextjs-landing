d9ab61ee1faed44d7297c209b57d52ec565a218b
Steven Del Rosario <stevendelro@pm.me>
2021-08-04 21:37:50 -0500

    updated index.js to add Demos section

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+
+import Hero from '../components/Sections/Hero';
+import About from '../components/Sections/About';
+import Demos from '../components/Sections/Demos';
+// import GetStarted from '../components/Sections/GetStarted';
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
+    <Container maxWidth= 'xl'>
+      <Hero />
+      <About />
+      <Demos />
+      {/* <GetStarted /> */}
     </Container>
   );
 }
