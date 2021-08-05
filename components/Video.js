81b9a11a7ebf17e80bf7fe0dcd2ee71be8fbbab6
Steven Del Rosario <stevendelro@pm.me>
2021-08-04 21:34:37 -0500

    created Video.js to handle demo videos

---
+++ components/Video.js
@@ -0,0 +1,49 @@
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+
+
+const useStyles = makeStyles(theme => ({
+  video__ROOT: {
+    verticalAlign: 'middle',
+    borderRadius: theme.shape.borderRadius,
+  },
+}))
+
+export default function Video({
+  imagePath,
+  videoPath,
+  height = 'auto',
+  width = '100%',
+}) {
+  const classes = useStyles()
+  const theme = useTheme()
+  const isTinyScreenDown = useMediaQuery(theme.breakpoints.down('xs'))
+  /**
+   * Video autoplay is disabled and video controls become visible on tiny
+   * screens.
+   *
+   * iPhones force a fullscreen letterbox mode when a video begins playing,
+   * causing the UX to become really confusing.
+   */
+  console.log(`imagePath`, imagePath)
+  return (
+    <video
+      className={classes.video__ROOT}
+      height={height}
+      width={width}
+      autoPlay={isTinyScreenDown ? false : true}
+      muted
+      controls={isTinyScreenDown ? false : true}
+      preload='auto'
+      poster={imagePath}
+      data-setup='{}'
+      loop>
+      <source src={videoPath} type='video/mp4'></source>
+      <p className='vjs-no-js'>
+        To view this video please enable JavaScript, and consider upgrading to a
+        web browser that supports HTML5 video. See:
+        https://videojs.com/html5-video-support/
+      </p>
+    </video>
+  )
+}
