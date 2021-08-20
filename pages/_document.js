9a9b278a27feef0d9337c814e721cfc9102d12d5
Steven Del Rosario <stevendelro@pm.me>
2021-08-20 11:22:47 -0500

    updated meta tags to include descriptions and a thumbnail image

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheets } from '@material-ui/core/styles';
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from '@material-ui/core/styles'
 
 export default class MyDocument extends Document {
   render() {
@@ -8,7 +8,28 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* PWA primary color */}
-          <meta name="theme-color" content='#fff' />
+          <meta name="theme-color" content="#fff" />
+          <meta
+            name="description"
+            content="A developer tool used to visualize Kubernetes topology and Prometheus derived metrics as customized graphs and tables"
+          />
+          <meta
+            name="title"
+            property="og:title"
+            content="KUR8 | A Visual Kubernetes/Prometheus Dashboard"
+          />
+          <meta property="og:type" content="website" />
+          <meta
+            name="image"
+            property="og:image"
+            content="https://priontyl.sirv.com/Images/kur8.png"
+          />
+          <meta
+            name="description"
+            property="og:description"
+            content="KUR8 | A Visual Kubernetes/Prometheus Dashboard"
+          />
+          <meta name="author" content="Team KUR8" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
@@ -19,7 +40,7 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
